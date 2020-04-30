0846aead7a23bf2628460df0e303d392f32ac93a
CelineTran <Celinetran7737@yahoo.com>
2020-04-29 21:57:12 -0700

    Create cppTest.cpp

---
+++ C++/cppTest.cpp
@@ -0,0 +1,24 @@
+ #include <iostream>
+#include <vector>
+#include<cstdlib>
+using namespace std;
+
+void populateArrays(vector<int> vect); 
+
+int main(){
+  int random = rand(); 
+  vector<int> one(100); 
+  vector<int> two(500); 
+  vector<int> three(1000); 
+
+}
+
+void populateArrays(vector<int> vect){
+  int half = vect.size()/2; 
+  for(int index = 0; index < half; ++index)
+    vect.at(index) = random(); 
+
+  for(int index = half; index < vect.size(); ++index)
+    vect.at(index) = (int)(((double)random()/RAND_MAX)* 1000000); 
+}
+
