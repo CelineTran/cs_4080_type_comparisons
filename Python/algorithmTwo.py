eab885dc6e8fbf82566037f28695be46eda318cd
kminhlee <51681056+kminhlee@users.noreply.github.com>
2020-05-04 15:51:10 -0700

    Algorithm 2 Finalization
    Finalized algorithm 2, which seeks to establish loss of data with widening conversions

--- Python/algorithmTwo.py
+++ Python/algorithmTwo.py
@@ -1,19 +1,26 @@
 import time
 import random
 
-def algorithmTwoInt(list1, list2, list3, listFin):
+def algorithmTwoInt(list1, list2, list3, listFinInt, listFinOrig):
 # Take in 4 arrays of same length and apply formula after 
 # converting to int at each index. Then fill listFin
     for idx in range(len(list1)):
         a = int(list1[idx])
         b = int(list2[idx])
         c = int(list3[idx])
-        listFin[idx] = -1*(a**2)+(b**2+c**2-4*a*c)/(2*a*c+c*a*b)
+        a2 = list1[idx]
+        b2 = list2[idx]
+        c2 = list3[idx]
+        listFinOrig[idx] = -1*(a2**2)+(b2**2+c**2-4*a*c2)/(2*a2*c2+c2*a2*b2)
+        listFinInt[idx] = int(-1*(a**2)+(b**2+c**2-4*a*c)/(2*a*c+c*a*b))
+        diff = abs(listFinOrig[idx]-listFinInt[idx])
+        print(f"Original Value: {listFinOrig[idx]}")
+        print(f"Pre-Typecasted Value: {listFinInt[idx]}")
+        print(f"Difference: {diff}\n")
 
 
 def main():
-    initTime = time.perf_counter()
-    listSize = 1000
+    listSize = 100
     # change list size as necessary
     list1 = []
     for i in range(0, listSize):
@@ -24,12 +31,12 @@ def main():
     list3 = []
     for i in range(0, listSize):
         list3.append(random.random()*1000+1)
-    listFin = []
+    listFinInt = []
     for i in range(0, listSize):
-        listFin.append(1)
-    algorithmTwoInt(list1, list2, list3, listFin)
-    #print(list)
-    finTime = time.perf_counter()
-    print(f"Completed in {finTime - initTime:0.5f} seconds")
+        listFinInt.append(1)
+    listFinOrig = []
+    for i in range(0, listSize):
+        listFinOrig.append(1)
+    algorithmTwoInt(list1, list2, list3, listFinInt, listFinOrig)
 
 main()
