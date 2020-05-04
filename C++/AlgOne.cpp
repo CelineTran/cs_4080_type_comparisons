ad0fd2eceac9458edca3bc5d7d03a67640520d37
CelineTran <Celinetran7737@yahoo.com>
2020-05-04 15:18:22 -0700

    Update AlgOne.cpp

--- C++/AlgOne.cpp
+++ C++/AlgOne.cpp
@@ -1,113 +1,112 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <time.h> 
 
 using namespace std;
 
-vector<int> intA(50); 
-vector<int> intB(250); 
-vector<int> intC(500); 
+vector<double> dblA(250); 
+vector<double> dblB(500); 
+vector<double> dblC(750); 
+vector<double> dblD(1000); 
 
-vector<double> dblA(50); 
-vector<double> dblB(250); 
-vector<double> dblC(500); 
-
-
-vector<int> intSort(vector<int> a, vector<double> b); 
-vector<double> doubleSort(vector<int> a, vector<double> b); 
 void populateVectors(); 
+void intSort(vector<double> a); //widening
+void doubleSort(vector<double> a); 
+double intTimeTrial(vector<double> a);
+double dblTimeTrial(vector<double> a);
 
 int main(){
-  int random = rand(); 
+  int random = rand();  
 
   populateVectors(); 
 
-  vector<int> sortedIntA = intSort(intA, dblA); 
-  vector<int> sortedIntB = intSort(intB, dblB);
-  vector<int> sortedIntC = intSort(intC, dblC); 
-
-  vector<double> sortedDblA = doubleSort(intA, dblA); 
-  vector<double> sortedDblB = doubleSort(intB, dblB); 
-  vector<double> sortedDblC = doubleSort(intC, dblC); 
+  cout << "With Type Coercion -- " << endl; 
+  cout << "Average time of 10 trials of size 250 is " << intTimeTrial(dblA)/CLOCKS_PER_SEC << " seconds" << endl; 
+  cout << "Average time of 10 trials of size 500 is " << intTimeTrial(dblB)/CLOCKS_PER_SEC << " seconds" << endl; 
+  cout << "Average time of 10 trials of size 750 is " << intTimeTrial(dblC)/CLOCKS_PER_SEC << " seconds" << endl; 
+  cout << "Average time of 10 trials of size 1000 is " << intTimeTrial(dblD)/CLOCKS_PER_SEC << " seconds" << endl;
+  cout << "Without Type Coercion -- " << endl; 
+  cout << "Average time of 10 trials of size 250 is " << intTimeTrial(dblA)/CLOCKS_PER_SEC << " seconds" << endl; 
+  cout << "Average time of 10 trials of size 500 is " << intTimeTrial(dblB)/CLOCKS_PER_SEC << " seconds" << endl; 
+  cout << "Average time of 10 trials of size 750 is " << intTimeTrial(dblC)/CLOCKS_PER_SEC << " seconds" << endl; 
+  cout << "Average time of 10 trials of size 1000 is " << intTimeTrial(dblD)/CLOCKS_PER_SEC << " seconds" << endl;  
   
 }
 
 void populateVectors(){
-  for(int index = 0; index < 50; ++index){
-    intA.at(index) = random(); 
-    dblA.at(index) = ((double)random()/RAND_MAX)* 1000000; 
-  
-  }
+  for(int i = 0; i < 250; ++i)
+    dblA.at(i) = 1001 * ((double)random()/(double)RAND_MAX); 
 
-  for(int index = 0; index < 250; ++index){
-    intB.at(index) = random(); 
-    dblB.at(index) = ((double)random()/RAND_MAX)* 1000000; 
-  }
+  for(int i = 0; i < 500; ++i)
+    dblB.at(i) = 1001 * ((double)random()/(double)RAND_MAX); 
 
-  for(int index = 0; index < 500; ++index){
-    intC.at(index) = random(); 
-    dblC.at(index) = ((double)random()/RAND_MAX)* 1000000; 
-  }
+  for(int i = 0; i < 750; ++i)
+    dblC.at(i) = 1001 * ((double)random()/(double)RAND_MAX); 
+
+  for(int i = 0; i < 1000; ++i)
+    dblD.at(i) = 1001 * ((double)random()/(double)RAND_MAX); 
 }
 
-vector<int> intSort(vector<int> a, vector<double> b){
+void intSort(vector<double> a){
   
-  vector<int> sortedVect(a.size()*2); 
-  vector<double> tempDblVect(b.size()); 
-  int k = 0;
+  vector<int> sortedVect(a.size()); 
+  vector<int> tempVect(a.size()); 
+  
+  for(int i = 0; i < a.size(); ++i)
+    tempVect.at(i) = (int)a.at(i); 
 
-  for(int i = 0; i < b.size(); i++){
-    int temp = (int)b.at(i); 
-    tempDblVect.at(i) = temp;
-  }
-
-  for (int i = 0; i < a.size(); ++i)
-    sortedVect.at(i) = a.at(i); 
-
-  for(int i = b.size(); i < b.size()*2 -1; ++i)
-    sortedVect.at(i) = tempDblVect[k++]; 
-
-  for(int i = 0; i < sortedVect.size()-1; ++i){
-    for(int j = 0; j < sortedVect.size() - i - 1; ++j){
-      if(sortedVect.at(j) > sortedVect.at(j+1)){
-        int temp = sortedVect.at(j); 
-        sortedVect.at(j) = sortedVect.at(j+1); 
+  for(int i = 0; i < tempVect.size()-1; ++i){
+    for(int j = 0; j < tempVect.size() - i - 1; ++j){
+      if(tempVect.at(j) > tempVect.at(j+1)){
+        int temp = tempVect.at(j); 
+        sortedVect.at(j) = tempVect.at(j+1); 
         sortedVect.at(j+1) = temp; 
       }
     }
   }
-
-  return sortedVect; 
 }
 
-vector<double> doubleSort(vector<int> a, vector<double> b){
+void doubleSort(vector<double> a){ 
 
-  vector<double> sortedVect(a.size()*2); 
-  vector<int> tempIntVect(b.size()); 
-  int k = 0;
+  vector<double> sortedVect(a.size()); 
 
-  for(int i = 0; i < a.size(); ++i){
-    double temp = (double)a.at(i); 
-    tempIntVect.at(i) = temp; 
-  }
-
-  for (int i = 0; i < b.size(); ++i)
-    sortedVect.at(i) = b.at(i); 
- 
-
-  for(int i = a.size(); i < a.size()*2 -1; ++i)
-    sortedVect.at(i) = tempIntVect[k++]; 
-
-  for(int i = 0; i < sortedVect.size()-1; ++i){
-    for(int j = 0; j < sortedVect.size() - i - 1; ++j){
-      if(sortedVect.at(j) > sortedVect.at(j+1)){
-        double temp = sortedVect.at(j); 
-        sortedVect.at(j) = sortedVect.at(j+1); 
+  for(int i = 0; i < a.size()-1; ++i){
+    for(int j = 0; j < a.size() - i - 1; ++j){
+      if(a.at(j) > a.at(j+1)){
+        double temp = a.at(j); 
+        sortedVect.at(j) = a.at(j+1); 
         sortedVect.at(j+1) = temp; 
       }
     }
   }
+}
 
-  return sortedVect; 
+double intTimeTrial(vector<double> a){
+  double avgTime = 0.0; 
+  clock_t start; 
+  clock_t end; 
+  for(int i = 0; i < 10; ++i){
+    start = clock(); 
+    intSort(a); 
+    end = clock(); 
+    avgTime += (end-start); 
+  }
+
+  return avgTime/10.0; 
+}
+
+double dblTimeTrial(vector<double> a){
+  double avgTime = 0.0; 
+  clock_t start; 
+  clock_t end; 
+  for(int i = 0; i < 10; ++i){
+    start = clock(); 
+    doubleSort(a); 
+    end = clock(); 
+    avgTime += (end-start); 
+  }
+
+  return avgTime/10.0; 
 }
 
